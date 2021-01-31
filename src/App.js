e22880b783eff1525f1e8f18dbcfeae4c4e1f34e
Nkwor-Jane <69125833+Nkwor-Jane@users.noreply.github.com>
2021-01-31 02:01:01 +0100

    A replica of the Dev challenges Error page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import scarecrow from './imgs/Scarecrow.png';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" w-full min-h-screen p-4 ">
+       <div >
+         <p className=" font-extrabold pl-4 md:pt-4 pt-2 ">404 NOT FOUND</p>
+     </div>
+      <div className="flex flex-col md:flex-row w-100 pt-10 items-center">
+      <div className=" w-full md:w-1/2 pl-10 ">
+          <img src={scarecrow} alt="left" className="w-full pr-6 pb-8 md:4/5"/>
+        </div>
+        <div className="w-100 md:w-1/2 pl-1 md:text-justify">
+          <h1 className="font-extrabold text-3xl md:text-5xl leading-7 md:pb-6 pb-4 md:leading-10">I have bad news<br/>
+          for you
+          </h1>
+          <p className="font-light md:font-medium text-1xl  md:text-2xl leading-6 md:leading-9 pb-12">The page you are looking 
+            for might be removed or is
+            temporarily unavailable.
+          </p>
+          <button className=" border-black bg-black text-white p-3">BACK TO HOMEPAGE</button>
+        </div>
+      </div>
+      <div className="pt-8 md:pt-10">
+       <p className="font-sm text-center ">Jane&copy;DevChallenges.io</p>
+      </div>
     </div>
   );
 }
